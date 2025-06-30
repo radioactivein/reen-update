4eba430bdda5ddafa3b3d9a1596059af5e216f3c
radioactivein <ameenbaasim@gmail.com>
2025-06-30 10:36:49 +0530

    updates

--- js/script.js
+++ js/script.js
@@ -7,7 +7,45 @@ $(document).ready(function(){
         // $('header nav ul').fadeIn();
     });
 
+    $('div').click(function(){
+        $('html, body').scrollTop(50);
+    })    
+
+    
+
+
+
 })
+
+
+
+
+
+
+$('p').click(function(){
+    // $(this).css("color","red");
+    // $(this).toggleClass("clicked");
+    // $(this).addClass("clicked");
+    // $(this).removeClass("clicked");
+    // $(this).removeClass("clicked");
+    // $('.feature').toggleClass("clicked");/
+    
+
+    // $('html, body').animate({
+    // scrollTop: 0
+    // }, 100);
+  
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
